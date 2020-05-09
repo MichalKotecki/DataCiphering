97e30c1e3624ef915ae3a5cdea2433d1837679ed
MichalKotecki <48054526+MichalKotecki@users.noreply.github.com>
2020-05-09 20:01:18 +0200

    Modular Inverse algorithm

--- ModularInverse/ModularInverse.py
+++ ModularInverse/ModularInverse.py
@@ -2,7 +2,7 @@
 # Author: Michał Kotecki
 # Date: 5/09/2020
 # Description:
-
+# This algorithm is used to find S in (a * S) mod b = 1, given that a and b are known.
 
 class TableRow:
     def __init__(self, q, r, s):
@@ -17,20 +17,15 @@ class TableRow:
         return f"q: {self.q}, r: {self.r}, s: {self.s} \n"
 
 
-# This is used to find S in (a S) mod b = 1 given that a and b are known.
+
 def modularInverse(a, b):
 
-    r0 = 0
-    r1 = 0
-    # TO-DO is this correct?
-    if (a > b):
-        r1 = 1
-    else:
-        r0 = 1
+    correctPositions = lambda a, b: (a, b, 1, 0) if a > b else (b, a, 0, 1)
+    r0, r1, s0, s1 = correctPositions(a,b)
 
     table = []
-    table.append(TableRow(0,a,r0))
-    table.append(TableRow(0,b,r1))
+    table.append(TableRow(0,r0,s0))
+    table.append(TableRow(0,r1,s1))
 
     while True:
         # calculating current row
@@ -41,18 +36,21 @@ def modularInverse(a, b):
         table.append(TableRow(q,r,s))
 
         if r == 1:
-            print(table)
-            return s if s > 0 else s + a
+            # print(table)
+            return s if s > 0 else s + b
         elif r == 0:
-            print(table)
+            # print(table)
             return -1
 
-    return -1
 
 if __name__ == '__main__':
-    a = 10
-    b = 7
 
     print("Modular Inverse Algorithm")
+    print("This is used to find S in (a * S) mod b = 1 given that a and b are known.")
+    print("Enter a:")
+    a = int(input())
+    print("Enter b:")
+    b = int(input())
+
     filterResult = lambda num: f"The solution is {num}." if num > 0 else "There is no solution."
     print(filterResult(modularInverse(a, b)))
